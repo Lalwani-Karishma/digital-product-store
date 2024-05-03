c0e63efe1871ba7243a52aabbf50d78e0875dca2
Lalwani-Karishma <lalwanikarishma216@gmail.com>
2024-05-03 17:01:34 +0530

    1.0

---
+++ data.js
@@ -0,0 +1,12 @@
+
+let a="Home";
+let b="About";
+let c="Sercive";
+let d="Premium";
+let e="Resorces";
+
+document.getElementById('a1').innerHTML=a;
+document.getElementById('a2').innerHTML=b;
+document.getElementById('a3').innerHTML=c;
+document.getElementById('a4').innerHTML=d;
+document.getElementById('a5').innerHTML=e;
